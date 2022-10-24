81065b750ff8bd8bf5ea0ed557b62356520bbca7
G91688 <gultacmansimova0493@gmail.com>
2022-10-25 00:25:13 +0200

    array

---
+++ js/h.js
@@ -0,0 +1,18 @@
+var seyahet = [
+    ["Tebiet",
+        ["Deniz","Hava limani"],"Ismayilli","Almaniya"
+    ],
+    ["Aytac","Gultac","Sema","Nubar","Azima-Esmira"],
+    ["Revan","Sebuhi","Rza","Deyanet"],
+    [
+        "Shah dag",["Yay","Qis"],"Mandarin",
+    ],
+    ["","Baki."]
+];
+
+console.log(seyahet[3][2][0]+seyahet[0][1][0][1]+seyahet[2][3][4]+
+    seyahet[3][1][1][1]+seyahet[0][3][2]+seyahet[3][0][4]+
+    seyahet[1][3][3]+seyahet[3][2][3]+seyahet[2][1][5]+seyahet[1][2][2]+
+    seyahet[0][1][1][4]+seyahet[1][1][0]+seyahet[2][1][3]+seyahet[0][2][6]+seyahet[2][3][6]+seyahet[3][0][2]+seyahet[1][1][5]+seyahet[1][4][5]+seyahet[3][0][5]+seyahet[2][1][5]+
+    seyahet[1][4][10]+seyahet[4][1][4]
+)
